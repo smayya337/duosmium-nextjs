e7d694320e3f3a5a84303ad2c7b882673a45cd4f
Shreyas Mayya <eav6vg@virginia.edu>
2023-04-18 10:18:41 -0400

    make cards look prettier

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -5,6 +5,7 @@ import { prisma } from '@/app/lib/global/prisma';
 import { Suspense } from 'react';
 import styles from './page.module.css';
 import Image from "next/image";
+import Link from "next/link";
 
 // @ts-ignore
 async function Card({ meta }) {
@@ -13,33 +14,39 @@ async function Card({ meta }) {
 	console.log(meta.duosmiumId);
 	return (
 		// @ts-ignore
-		<div className={`mdc-card`} style={{ '--mdc-text-button-label-text-color': meta.color, '--mdc-filled-button-container-color': meta.color, '--mdc-text-button-hover-state-layer-color': meta.color }}>
-			<div className="mdc-card__primary-action" tabIndex={0}>
-				<div className={`mdc-card__content ${styles.cardHeader}`}>
-					<h6 className={`${styles.cardTitle} mdc-typography--headline6`}>{fullTournamentTitle(interpreter.tournament)}</h6>
-					<p className={`${styles.cardDate} mdc-typography--subtitle1`}>{dateString(interpreter)}</p>
-					<p className={`${styles.cardLocation} mdc-typography--subtitle1`}>@ {interpreter.tournament.location}</p>
-				</div>
-				<div className="mdc-card__media mdc-card__media--16-9">
-					<div
-						className={`mdc-card__media-content ${styles.mdcCard__mediaContent}`}>
-						<Image src={meta.logo} alt={`Logo for the ${fullTournamentTitle(interpreter.tournament)}`} fill={true} className={styles.cardImage} />
+		<div className="mdc-card" style={{ '--mdc-theme-primary': meta.color }}>
+			<Link href={`/results/${meta.duosmiumId}`} className={styles.primaryCardLink}>
+				<div className="mdc-card__primary-action" tabIndex={0}>
+					<div className={`mdc-card__content ${styles.cardHeader}`}>
+						<h6 className={`${styles.cardTitle} mdc-typography--headline6`}>{fullTournamentTitle(interpreter.tournament)}</h6>
+						<p className={`${styles.cardDate} mdc-typography--subtitle1`}>{dateString(interpreter)}</p>
+						<p className={`${styles.cardLocation} mdc-typography--subtitle1`}>@ {interpreter.tournament.location}</p>
 					</div>
+					<div className="mdc-card__media mdc-card__media--16-9">
+						<div
+							className={`mdc-card__media-content ${styles.mdcCard__mediaContent}`}>
+							<Image src={meta.logo} alt={`Logo for the ${fullTournamentTitle(interpreter.tournament)}`} fill={true} className={styles.cardImage} />
+						</div>
+					</div>
+					<div className="mdc-card__ripple" />
 				</div>
-				<div className="mdc-card__ripple" />
-			</div>
+			</Link>
 			<div className="mdc-card__actions">
 				<div className="mdc-card__action-buttons">
 					<button className="mdc-button mdc-card__action mdc-card__action--button">
 						<div className="mdc-button__ripple" />
 						<span className="mdc-button__label">Summary</span>
 					</button>
-					<button className="mdc-button mdc-card__action mdc-card__action--button">
-						<div className="mdc-button__ripple" />
-						<span className="mdc-button__label">Full Results</span>
-					</button>
-					<button className="mdc-button mdc-button--unelevated">
-						<div className="mdc-button__ripple" />
+					<Link href={`/results/${meta.duosmiumId}`}>
+						<button className="mdc-button mdc-card__action mdc-card__action--button" tabIndex={-1}>
+							<div className="mdc-button__ripple" />
+							<span className="mdc-button__label">Full Results</span>
+						</button>
+					</Link>
+				</div>
+				<div className="mdc-card__action-icons">
+					<button className={`mdc-button mdc-button--unelevated ${styles.teamCountButton}`}>
+						{/*<div className="mdc-button__ripple" />*/}
 						<span className="mdc-button__label">
 							{interpreter.tournament.nonExhibitionTeamsCount} Teams
 						</span>
