d84c037d0dc6bfa2384efa121aae597d56e953ac
Shreyas Mayya <smayya337@gmail.com>
2023-04-18 21:50:27 -0400

    use Google Fonts link instead of next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,13 @@
 import Script from 'next/script';
-import { Roboto } from 'next/font/google';
 import React from 'react';
 
 export const metadata = {
 	title: 'Duosmium Results'
 };
 
-const roboto = Roboto({
-	weight: ['400', '500', '700'],
-	style: ['normal', 'italic'],
-	subsets: ['latin'],
-	display: 'swap'
-});
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className={roboto.className}>
+		<html lang="en">
 			<head>
 				<link
 					href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css"
@@ -27,6 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
 				/>
+						<link
+							href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
+							rel="stylesheet" />
 				<Script
 					src={
 						'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js'
