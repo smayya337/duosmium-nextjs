deecfa1a2d8d5d395c296f385c475a2991d1a844
Shreyas Mayya <smayya337@gmail.com>
2023-05-23 16:10:23 -0400

    add back upload functionality for admins

--- app/results/upload/page.tsx
+++ app/results/upload/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import { getServerComponentSupabaseClient } from "@/lib/global/supabase";
+import { cookies, headers } from "next/headers";
+import { getCurrentUserID } from "@/lib/auth/helpers";
+import { isAdmin } from "@/lib/auth/admin";
+import { redirect } from "next/navigation";
+
+export default async function Upload() {
+	const supabase = getServerComponentSupabaseClient(headers, cookies);
+	const userID = await getCurrentUserID(supabase);
+	if (!(await isAdmin(userID))) {
+		redirect("/results");
+	}
 	return (
 		<form action="/results/upload/submit" method="POST" encType="multipart/form-data">
-			<label htmlFor="yaml">Upload YAML file:</label>
+			<label htmlFor="yaml">Upload YAML file: </label>
 			<input type="file" name="yaml" id="yaml" accept=".yaml,.yml" multiple />
 			<br />
 			<input type="submit" value="Submit" />
--- app/results/upload/submit/route.ts
+++ app/results/upload/submit/route.ts
@@ -1,22 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { ResultsAddQueue } from '@/lib/results/queue';
+import { createRouteHandlerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { cookies, headers } from "next/headers";
+import { getCurrentUserID } from "@/lib/auth/helpers";
+import { isAdmin } from "@/lib/auth/admin";
 
 export async function POST(request: NextRequest) {
-	return new NextResponse(null, { status: 405 });
-	// const data = await request.formData();
-	// const allFiles = data.getAll('yaml');
-	// const q = ResultsAddQueue.getInstance();
-	// q.drain(function () {
-	// 	console.log('All results have been added!');
-	// });
-	// for (const file of allFiles) {
-	// 	if (file === null || typeof file === 'string') {
-	// 		new NextResponse('Uploaded value is not a file!', { status: 400 });
-	// 	} else {
-	// 		// noinspection ES6MissingAwait
-	// 		q.push(file);
-	// 		// addResultFromYAMLFile(file);
-	// 	}
-	// }
-	// return NextResponse.redirect(`${new URL(request.url).host}/results/upload`, 303);
+	const supabase = createRouteHandlerSupabaseClient({
+		headers,
+		cookies
+	});
+	const user = await getCurrentUserID(supabase);
+	if (user === null) {
+		return new Response(null, { status: 401 });
+	} else if (!(await isAdmin(user))) {
+		return new Response(null, { status: 403 });
+	}
+	// return new NextResponse(null, { status: 405 });
+	const data = await request.formData();
+	const allFiles = data.getAll('yaml');
+	const q = ResultsAddQueue.getInstance();
+	q.drain(function () {
+		console.log('All results have been added!');
+	});
+	for (const file of allFiles) {
+		if (file === null || typeof file === 'string') {
+			new NextResponse('Uploaded value is not a file!', { status: 400 });
+		} else {
+			// noinspection ES6MissingAwait
+			q.push(file);
+			// addResultFromYAMLFile(file);
+		}
+	}
+	return NextResponse.redirect(`${request.nextUrl.host}/results/upload`, 303);
 }
